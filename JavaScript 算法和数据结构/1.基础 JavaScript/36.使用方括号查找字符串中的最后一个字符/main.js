f15cfdb14fde8a7a67ebf470747de8754fcfe52a
chenhaizhu <394279480@qq.com>
2023-02-21 00:04:02 +0800

    JavaScript-使用方括号查找字符串中的最后一个字符

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/36.使用方括号查找字符串中的最后一个字符/main.js
@@ -0,0 +1,7 @@
+// DO: 使用方括号表示法（ bracket notation）来找到 lastName 变量中的最后一个字符。
+
+// 设置
+const lastName = "Lovelace";
+
+// 只修改这一行下面的代码
+const lastLetterOfLastName = lastName[lastName.length - 1]; // 修改这一行
