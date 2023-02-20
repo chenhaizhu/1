21d2819ab08b5d7ca19301652cebba50428b8074
chenhaizhu <394279480@qq.com>
2023-02-20 23:56:16 +0800

    JavaScript-使用方括号查找字符串中的第一个字符

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/33.使用方括号查找字符串中的第一个字符/main.js
@@ -0,0 +1,8 @@
+// DO: 使用方括号获取变量 lastName 中的第一个字符，并赋给变量 firstLetterOfLastName。
+
+// 设置
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// 只修改这一行下面的代码
+firstLetterOfLastName = lastName[0]; // 修改这一行
