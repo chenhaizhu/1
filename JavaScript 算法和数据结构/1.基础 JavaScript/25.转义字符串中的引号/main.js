eebef664e298807f401a2f4047b77262e4a48286
FAIR131 <1318632741@qq.com>
2023-02-20 22:59:15 +0800

    JavaScript-转义字符串中的引号

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/25.转义字符串中的引号/main.js
@@ -0,0 +1,4 @@
+// DO: 使用反斜杠给 myStr 变量赋值一个字符串，这样如果你要打印它到控制台，将会看到：
+// I am a "double quoted" string inside "double quotes".
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // 修改这一行
