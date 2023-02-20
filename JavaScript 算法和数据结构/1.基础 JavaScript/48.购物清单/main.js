b7bc6339353a81cf9b4761efe907a97db0c1b5ba
chenhaizhu <394279480@qq.com>
2023-02-21 00:45:23 +0800

    JavaScript-购物清单

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/48.购物清单/main.js
@@ -0,0 +1,10 @@
+// DO: 创建一个名叫 myList 的购物清单。 清单的数据格式就是多维数组。
+// 每个子数组中的第一个元素应该是购买的物品名称。 第二个元素应该是物品的数量，类似于： ["Chocolate Bar", 15]
+// 列表中应至少有 5 个子数组。
+
+const myList = [];
+myList.push(["Apple", 1]);
+myList.push(["Pen", 2]);
+myList.push(["Desk", 3]);
+myList.push(["Cup", 4]);
+myList.push(["Phone", 5]);
