12ad7f1f235c2d8ff8235c21276be82d75cd6465
FAIR131 <1318632741@qq.com>
2023-02-20 20:34:42 +0800

    JavaScript-了解变量名区分大小写

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/8.了解变量名区分大小写/main.js
@@ -0,0 +1,12 @@
+// DO: 修改已声明的变量，让它们的命名符合驼峰命名法（camelCase）的规范。
+// 不要创建任何新变量。
+
+// 变量声明
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// 变量赋值
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
