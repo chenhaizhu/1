d9c44d49077e270c4d53cfb463f799386808dd77
FAIR131 <1318632741@qq.com>
2023-02-20 20:45:42 +0800

    JavaScript-使用 const 关键字声明只读变量

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/10.使用 const 关键字声明只读变量/main.js
@@ -0,0 +1,7 @@
+// DO: 更改代码，以便使用 let 或 const 声明所有变量。 当你想要改变变量时使用 let，当你想要变量保持不变时使用 const。 此外，重命名使用 const
+// 声明的变量以符合惯例。 请勿更改分配给变量的字符串。
+
+const FCC = "freeCodeCamp"; // 修改这一行
+let fact = "is cool!"; // 修改这一行
+fact = "is awesome!";
+console.log(FCC, fact); // 修改这一行
