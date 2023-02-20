5576de48976537f80c124b23c01c848e94baf994
chenhaizhu <394279480@qq.com>
2023-02-21 00:13:15 +0800

    JavaScript-填词造句

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/38.填词造句/main.js
@@ -0,0 +1,13 @@
+// DO: 在这个挑战中，我们会提供给你一个名词、一个动词、一个形容词和一个副词。 你需要在我们提供的词语中，选择你要使用的词语来使这个句子变得完整。
+// 你需要使用字符串连接操作符 + 和提供的变量 myNoun、myAdjective、myVerb 和 myAdverb 来构建一个新的字符串。
+// 然后，将这个改好的新字符串赋值给 wordBlanks 变量。 不要去更改已经赋值给这些变量的单词。
+// 你还需要确认字符串里的空格，以确保最后的句子里的每个单词之间都有空格。 最后的结果应该是一个完整的句子。
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// 只修改这一行下面的代码
+const wordBlanks = myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // 修改这一行
+// 只修改这一行上面的代码
