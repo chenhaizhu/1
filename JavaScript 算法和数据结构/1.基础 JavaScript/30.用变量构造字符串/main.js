773578f2b17b17c7b008e5ee4062537fe1189491
chenhaizhu <394279480@qq.com>
2023-02-20 23:45:14 +0800

    JavaScript-用变量构造字符串

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/30.用变量构造字符串/main.js
@@ -0,0 +1,5 @@
+// DO: 把你的名字赋值给变量 myName，然后把变量 myName 插入到字符串 My name is 和 and I am well! 之间，并把连接后的结果赋值给变量 myStr。
+
+// 只修改这一行下面的代码
+const myName = "CaiXuKun";
+const myStr = "My name is " + myName + " and I am well!";
