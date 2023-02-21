24a7ebe80722a3bc4c89dcee0f8845044f25ed70
chenhaizhu <394279480@qq.com>
2023-02-21 23:33:55 +0800

    JavaScript-使用返回值赋值

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/56.使用返回值赋值/main.js
@@ -0,0 +1,11 @@
+// DO: 调用 processArg 函数，参数为 7，然后把返回的值赋值给变量 processed。
+
+// 设置
+let processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+
+// 只修改这一行下面的代码
+processed = processArg(7);
