ffa586e4ea68fc5a0aaf4accbfde2de77529de43
chenhaizhu <394279480@qq.com>
2023-02-21 23:27:10 +0800

    JavaScript-函数中的全局作用域和局部作用域

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/54.函数中的全局作用域和局部作用域/main.js
@@ -0,0 +1,13 @@
+// DO: 给 myOutfit 添加一个局部变量来将 outerWear 的值重载为 sweater。
+
+// 设置
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+    // 只修改这一行下面的代码
+    const outerWear = "sweater";
+    // 只修改这一行上面的代码
+    return outerWear;
+}
+
+myOutfit();
