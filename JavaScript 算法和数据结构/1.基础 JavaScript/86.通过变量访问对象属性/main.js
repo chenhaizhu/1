5089b2ff13a8f718a73dee0739d0064eaa351b62
chenhaizhu <394279480@qq.com>
2023-02-23 21:54:14 +0800

    JavaScript-通过变量访问对象属性

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/86.通过变量访问对象属性/main.js
@@ -0,0 +1,12 @@
+// DO: 将 playerNumber 变量设置为 16。 然后，使用该变量查找播放器的名称并将其分配给 player。
+
+// 设置
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+
+// 只修改这一行下面的代码
+const playerNumber = 16;  // 修改这一行
+const player = testObj[playerNumber];   // 修改这一行
