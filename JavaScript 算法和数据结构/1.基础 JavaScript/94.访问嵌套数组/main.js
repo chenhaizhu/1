92a21a868b134f975bc7ba36becf9e9dbe7804e9
chenhaizhu <394279480@qq.com>
2023-02-23 22:30:35 +0800

    JavaScript-访问嵌套数组

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/94.访问嵌套数组/main.js
@@ -0,0 +1,22 @@
+// DO: 使用点和方括号，将变量 secondTree 的值设置为 myPlants 对象中 trees 列表的第二个项目。
+
+const myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+const secondTree = myPlants[1].list[1];
