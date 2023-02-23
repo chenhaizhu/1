3a5b974c5ef730ec40abfca9531bfb0145718128
chenhaizhu <394279480@qq.com>
2023-02-23 22:01:48 +0800

    JavaScript-删除对象的属性

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/89.删除对象的属性/main.js
@@ -0,0 +1,13 @@
+// DO: 删除 myDog 对象的 tails 属性。 可以使用点操作符或者中括号操作符。
+
+// 设置
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+
+// 只修改这一行下面的代码
+delete myDog.tails;
