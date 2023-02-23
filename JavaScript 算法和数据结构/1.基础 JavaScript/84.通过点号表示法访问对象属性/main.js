8fe9bfd20d4f54e121b49ba71f95350ed7d4c4d0
chenhaizhu <394279480@qq.com>
2023-02-23 21:48:12 +0800

    JavaScript-通过点号表示法访问对象属性

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/84.通过点号表示法访问对象属性/main.js
@@ -0,0 +1,13 @@
+// DO: 使用点号读取 testObj 的属性值。
+// 将变量 hatValue 的值设置为该对象的 hat 属性的值，并将变量 shirtValue 的值设置为该对象的 shirt 属性的值。
+
+// 设置
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+// 只修改这一行下面的代码
+const hatValue = testObj.hat;      // 修改这一行
+const shirtValue = testObj.shirt;    // 修改这一行
