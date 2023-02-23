43b68080b0777831d8743e7d2b8a721359a406b7
chenhaizhu <394279480@qq.com>
2023-02-23 23:09:21 +0800

    JavaScript-使用 For 循环遍历数组

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/100.使用 For 循环遍历数组/main.js
@@ -0,0 +1,10 @@
+// DO: 声明并初始化一个变量 total 值为 0。 使用 for 循环，使得 total 的值为 myArr 的数组中的每个元素的值的总和。
+
+// 设置
+const myArr = [2, 3, 4, 5, 6];
+
+// 只修改这一行下面的代码
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+    total += myArr[i];
+}
