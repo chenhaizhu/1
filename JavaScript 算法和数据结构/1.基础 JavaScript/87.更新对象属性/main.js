39d72cf6584a31aca8e3feaeae5fe9e6a1165beb
chenhaizhu <394279480@qq.com>
2023-02-23 21:57:25 +0800

    JavaScript-更新对象属性

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/87.更新对象属性/main.js
@@ -0,0 +1,12 @@
+// DO: 更新 myDog 对象的 name 属性。 让它的名字从 Coder 变成 Happy Coder。 你可以使用点号表示法或方括号表示法来完成此挑战。
+
+// 设置
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+
+// 只修改这一行下面的代码
+myDog.name = "Happy Coder";
