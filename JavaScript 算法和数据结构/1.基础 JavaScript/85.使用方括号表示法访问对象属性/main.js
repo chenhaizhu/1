6676a886ea66ef76af3c5b3842512568688a7392
chenhaizhu <394279480@qq.com>
2023-02-23 21:51:29 +0800

    JavaScript-使用方括号表示法访问对象属性

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/85.使用方括号表示法访问对象属性/main.js
@@ -0,0 +1,12 @@
+// DO: 使用方括号读取 testObj 中 an entree 和 the drink 的属性值，并分别将它们赋值给 entreeValue 和 drinkValue。
+
+// 设置
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+};
+
+// 只修改这一行下面的代码
+const entreeValue = testObj["an entree"];   // 修改这一行
+const drinkValue = testObj["the drink"];    // 修改这一行
