f37e24b18e8260faaad20fdf5f9373891c2214dd
chenhaizhu <394279480@qq.com>
2023-02-23 22:23:48 +0800

    JavaScript-操作复杂对象

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/92.操作复杂对象/main.js
@@ -0,0 +1,24 @@
+// DO: 添加一个新专辑到 myMusic 数组。 添加 artist 和 title 字符串，release_year 数字和 formats 字符串数组。
+
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "111",
+        "title": "www",
+        "release_year": 1988,
+        "formats": [
+            "DSD",
+            "8s"
+        ]
+    }
+];
