ac079b354b158c942560021e27b96332e3d68027
chenhaizhu <394279480@qq.com>
2023-02-23 23:17:57 +0800

    JavaScript-do...while 循环

---
+++ JavaScript 算法和数据结构/1.基础 JavaScript/102.do...while 循环/main.js
@@ -0,0 +1,11 @@
+// DO: 将代码中的 while 循环更改为 do...while 循环，将数字 10 添加到 myArray 中，代码执行完时，i 等于 11。
+
+// 设置
+const myArray = [];
+let i = 10;
+
+// 只修改这一行下面的代码
+do {
+    myArray.push(i);
+    i++;
+} while (i < 11)
