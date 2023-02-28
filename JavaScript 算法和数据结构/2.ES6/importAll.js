28d16c105540d24d4cc6b5fc87263b6772af44f3
chenhaizhu <394279480@qq.com>
2023-03-01 00:15:58 +0800

    ES6-用 * 从文件中导入所有内容

---
+++ JavaScript 算法和数据结构/2.ES6/importAll.js
@@ -0,0 +1,18 @@
+// 下面的代码需要从同目录下的 string_functions.js 文件中导入所有内容。 使用 import * as 语法将文件的所有内容导入对象 stringFunctions。
+
+/*
+
+// 只修改这一行上面的代码
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+*/
+
+//            ↓
+
+import * as stringFunctions from "./string_functions.js";
+// 只修改这一行上面的代码
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+
