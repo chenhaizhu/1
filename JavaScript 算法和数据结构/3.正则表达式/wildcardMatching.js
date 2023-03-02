6970f57678866b867d62db327a98a9c1482e018c
chenhaizhu <394279480@qq.com>
2023-03-02 23:34:18 +0800

    ES6-用通配符匹配任何内容

---
+++ JavaScript 算法和数据结构/3.正则表达式/wildcardMatching.js
@@ -0,0 +1,13 @@
+// 完成正则表达式 unRegex 以匹配字符串 run、sun、fun、pun、nun 和 bun。 正则表达式中应该使用通配符。
+/*
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /change/; // 修改这一行
+let result = unRegex.test(exampleStr);
+*/
+
+//           ↓
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // 修改这一行
+let result = unRegex.test(exampleStr);
+
