681eb7384c8c3dc924f69c30febde7d7ddd839f8
chenhaizhu <394279480@qq.com>
2023-03-13 02:21:23 +0800

    调试 - 使用 type of 检查变量的类型

---
+++ JavaScript 算法和数据结构/4.调试/checkVariableTypes.js
@@ -0,0 +1,18 @@
+// 添加两个 console.log() 语句来检查代码中的两个变量 seven 和 three 的 typeof 值。
+
+
+/*
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// 只修改这一行下面的代码
+*/
+
+//               ↓
+
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// 只修改这一行下面的代码
+console.log(typeof seven);
+console.log(typeof three);
