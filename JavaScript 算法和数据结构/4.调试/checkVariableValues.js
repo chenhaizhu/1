2050c2248e57050bd40cd37e12212d51e656b07f
chenhaizhu <394279480@qq.com>
2023-03-13 02:11:55 +0800

    调试 - 使用控制台检查变量值

---
+++ JavaScript 算法和数据结构/4.调试/checkVariableValues.js
@@ -0,0 +1,24 @@
+// 使用 console.log() 方法打印代码中记录的变量 a 的值。
+
+
+/*
+let a = 5;
+let b = 1;
+a++;
+// 只修改这一行下面的代码
+
+
+let sumAB = a + b;
+console.log(sumAB);
+*/
+
+//               ↓
+
+let a = 5;
+let b = 1;
+a++;
+// 只修改这一行下面的代码
+console.log(a);
+
+let sumAB = a + b;
+console.log(sumAB);
