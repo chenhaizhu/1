512f1da9b1b09e8db1a4932735de0908315c5660
chenhaizhu <394279480@qq.com>
2023-03-13 02:27:53 +0800

    调试 - 捕获拼错的变量名和函数名

---
+++ JavaScript 算法和数据结构/4.调试/catchWrongNames.js
@@ -0,0 +1,16 @@
+// 修复代码中的两个拼写错误，以便 netWorkingCapital 计算有效。
+
+
+/*
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = recievables - payable;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+*/
+
+//               ↓
+
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
