147675e830da6e651763a02cf5e10a21ff934e6d
chenhaizhu <394279480@qq.com>
2023-03-14 01:02:04 +0800

    基础数据结构：使用数组存储不同类型的数据

---
+++ JavaScript 算法和数据结构/5.基础数据结构/mixedTypeArray.js
@@ -0,0 +1,11 @@
+// 我们已经定义了一个名为 yourArray 的变量。 请修改代码，将一个含有至少 5 个元素的数组赋值给 yourArray 变量。
+// 你的数组中应包含至少一个 string 类型的数据、一个 number 类型的数据和一个 boolean 类型的数据。
+
+/*
+let yourArray; // 修改这一行
+*/
+
+//               ↓
+
+let yourArray; // 修改这一行
+yourArray = ['string', 1, true, undefined, null];
