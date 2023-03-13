80854b27cfe4dc72a8098f35d53b4d006cf6a5ac
chenhaizhu <394279480@qq.com>
2023-03-13 22:30:39 +0800

    调试 - 调用函数时，捕获以错误顺序传递的参数

---
+++ JavaScript 算法和数据结构/4.调试/argSequenceChecker.js
@@ -0,0 +1,24 @@
+// 函数 raiseToPower 返回基数 (base) 的指数 (exponent) 次幂。 不幸的是，它没有被正确调用 — 修改代码，使 power 的值为 8。
+
+
+/*
+function raiseToPower(b, e) {
+  return Math.pow(b, e);
+}
+
+let base = 2;
+let exp = 3;
+let power = raiseToPower(exp, base);
+console.log(power);
+*/
+
+//               ↓
+
+function raiseToPower(b, e) {
+    return Math.pow(b, e);
+}
+
+let base = 2;
+let exp = 3;
+let power = raiseToPower(base, exp);
+console.log(power);
