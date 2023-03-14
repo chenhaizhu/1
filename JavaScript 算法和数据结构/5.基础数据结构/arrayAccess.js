32193f52bc9006abfa334b1b45bed285c66dfe73
chenhaizhu <394279480@qq.com>
2023-03-14 21:57:20 +0800

    基础数据结构：使用方括号访问数组的元素

---
+++ JavaScript 算法和数据结构/5.基础数据结构/arrayAccess.js
@@ -0,0 +1,17 @@
+// 将 myArray 中的第二个元素（索引为 1）设置为除了 b 以外的任意值。
+
+/*
+let myArray = ["a", "b", "c", "d"];
+// 只修改这一行下面的代码
+
+// 只修改这一行上面的代码
+console.log(myArray);
+*/
+
+//               ↓
+
+let myArray = ["a", "b", "c", "d"];
+// 只修改这一行下面的代码
+myArray[1] = "e";
+// 只修改这一行上面的代码
+console.log(myArray);
