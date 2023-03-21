646a3bb7647e48036a1d29e9b218bff412ea6148
chenhaizhu <394279480@qq.com>
2023-03-22 00:36:41 +0800

    面向对象编程 - 定义构造函数

---
+++ JavaScript 算法和数据结构/7.面向对象编程/defineConstructor.js
@@ -0,0 +1,11 @@
+// 创建一个构造函数：Dog。 给其添加 name，color 和 numLegs 属性并分别给它们设置为：字符串、字符串和数字
+/*
+*/
+
+//               ↓
+
+function Dog() {
+    this.name = "Jack";
+    this.color = "Black";
+    this.numLegs = 4;
+}
