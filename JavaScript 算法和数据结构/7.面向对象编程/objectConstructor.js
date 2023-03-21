8826c661e49d2d781724b01b666bad102efbe20d
chenhaizhu <394279480@qq.com>
2023-03-22 00:45:44 +0800

    面向对象编程 - 使用构造函数创建对象

---
+++ JavaScript 算法和数据结构/7.面向对象编程/objectConstructor.js
@@ -0,0 +1,20 @@
+// 使用上一个挑战中的 Dog 构造函数创建一个 Dog 的新实例，并把它赋值给变量 hound。
+/*
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+// 只修改这一行下面的代码
+*/
+
+//               ↓
+
+function Dog() {
+    this.name = "Rupert";
+    this.color = "brown";
+    this.numLegs = 4;
+}
+
+// 只修改这一行下面的代码
+let hound = new Dog();
