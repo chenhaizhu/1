b0f274a2293049fe3087ac6632dec45f052f6797
chenhaizhu <394279480@qq.com>
2023-03-22 00:22:27 +0800

    面向对象编程 - 使用点符号来访问对象的属性

---
+++ JavaScript 算法和数据结构/7.面向对象编程/dotAccess.js
@@ -0,0 +1,18 @@
+// 请在控制台里面输出 dog 对象中两个属性的值。
+/*
+let dog = {
+  name: "Spot",
+  numLegs: 4
+};
+// 只修改这一行下面的代码
+*/
+
+//               ↓
+
+let dog = {
+    name: "Spot",
+    numLegs: 4
+};
+// 只修改这一行下面的代码
+console.log(dog.name);
+console.log(dog.numLegs);
